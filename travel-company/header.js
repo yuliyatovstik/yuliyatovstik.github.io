21281935fffac60cd2b969141ce9e7c11fdb1693
Yuliya Tovstik <fegora3@gmail.com>
2020-07-18 20:18:43 +0300

    header fix

--- travel-company/header.js
+++ travel-company/header.js
@@ -9,19 +9,17 @@ document.addEventListener("DOMContentLoaded", function() {
 //if($(window).width() > 751) {
     $(window).scroll(function () {
         // console.log($(window).scrollTop());
-        if ($(window).scrollTop() > 100) {
-            $("header").css("background", "rgba(0, 0, 0, 0.85)")
-        } else {
-            $("header").css("background", "rgba(0, 0, 0, 0)")
+        if ($(window).scrollTop() > 50) {
+            $("header").css("background", "#3B4750")
         }
     })
 //}
-if($(window).width() < 769) {
-    $("<li>contact us</li>").appendTo(".navigation__list").addClass("navigation__item");
+// if($(window).width() < 769) {
+//     $("<li><div>contact us</div></li>").appendTo(".navigation__list").addClass("navigation__button").addClass("navigation__contact-us").css("display", "flex");
     // let list = document.querySelector(".navigation__list");
     // let li = document.createElement("li");
     // li.classList.add("navigation__item");
     // li.innerHTML = "contact us";
     // list.append(li);
     // console.log(li);  
-}
+// }
