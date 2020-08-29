680f24b3daf5d3f86807a7c86be8796da531a161
Yuliya Tovstik <fegora3@gmail.com>
2020-08-29 15:39:22 +0300

    burger react

---
+++ react-burger/precache-manifest.aa547364d8011298f7f45a48e2f7f91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bd845843000b0524577f33a384e96f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "088a058194b3192b391f",
+    "url": "/static/css/main.a9e280f1.chunk.css"
+  },
+  {
+    "revision": "5fcaf852e13265bec8c1",
+    "url": "/static/js/2.1b515a4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b515a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "088a058194b3192b391f",
+    "url": "/static/js/main.c10c385b.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
